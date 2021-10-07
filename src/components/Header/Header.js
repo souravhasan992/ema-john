4d44da16b53b863260dfc8a809c8b12beac11a77
souravhasan992 <souravhasan992@gmail.com>
2021-10-07 13:30:57 -0500

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from '../../images/logo.png'
+import './Header.css'
 const Header = () => {
     return (
-        <div>
-            <h2>This is header</h2>
-            <img src={logo} alt="" />
+        <div className='header'>
+            <img className='logo' src={logo} alt="" />
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/orders">Orders</a>
+                <a href="/inventory">Manage Inventory</a>
+                </nav>
         </div>
     );
 };
